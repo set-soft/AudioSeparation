e7221bce0e5ba5b1b0a0fb0f6abe1a1c46db8686
Salvador E. Tropea <stropea@inti.gob.ar>
2025-07-06 14:46:25 -0300

    [Added] Demucs code from Meta

---
+++ source/inference/demucs_code.py
@@ -0,0 +1,94 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the license found in the
+# LICENSE file in the root directory of this source tree.
+# License: MIT
+# Misc stuff used by Demucs
+import functools
+import math
+import torch
+from torch.nn import functional as F
+import typing as tp
+
+
+# ############################################################################################################################
+# utils.py
+# ############################################################################################################################
+
+
+def unfold(a, kernel_size, stride):
+    """Given input of size [*X, T], output Tensor of size [*X, F, K]
+    with K the kernel size, by extracting frames with the given stride.
+
+    This will pad the input so that `F = ceil(T / K)`.
+
+    see https://github.com/pytorch/pytorch/issues/60466
+    """
+    *shape, length = a.shape
+    n_frames = math.ceil(length / stride)
+    tgt_length = (n_frames - 1) * stride + kernel_size
+    a = F.pad(a, (0, tgt_length - length))
+    strides = list(a.stride())
+    assert strides[-1] == 1, 'data should be contiguous'
+    strides = strides[:-1] + [stride, 1]
+    return a.as_strided([*shape, n_frames, kernel_size], strides)
+
+
+def center_trim(tensor: torch.Tensor, reference: tp.Union[torch.Tensor, int]):
+    """
+    Center trim `tensor` with respect to `reference`, along the last dimension.
+    `reference` can also be a number, representing the length to trim to.
+    If the size difference != 0 mod 2, the extra sample is removed on the right side.
+    """
+    ref_size: int
+    if isinstance(reference, torch.Tensor):
+        ref_size = reference.size(-1)
+    else:
+        ref_size = reference
+    delta = tensor.size(-1) - ref_size
+    if delta < 0:
+        raise ValueError("tensor must be larger than reference. " f"Delta is {delta}.")
+    if delta:
+        tensor = tensor[..., delta // 2:-(delta - delta // 2)]
+    return tensor
+
+
+class DummyPoolExecutor:
+    class DummyResult:
+        def __init__(self, func, *args, **kwargs):
+            self.func = func
+            self.args = args
+            self.kwargs = kwargs
+
+        def result(self):
+            return self.func(*self.args, **self.kwargs)
+
+    def __init__(self, workers=0):
+        pass
+
+    def submit(self, func, *args, **kwargs):
+        return DummyPoolExecutor.DummyResult(func, *args, **kwargs)
+
+    def shutdown(self, wait=True, cancel_futures=True):
+        return
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_value, exc_tb):
+        return
+
+
+# ############################################################################################################################
+# state.py
+# ############################################################################################################################
+
+
+def capture_init(init):
+    @functools.wraps(init)
+    def __init__(self, *args, **kwargs):
+        self._init_args_kwargs = (args, kwargs)
+        init(self, *args, **kwargs)
+
+    return __init__
--- source/inference/stft.py
+++ source/inference/stft.py
@@ -162,3 +162,51 @@ def stft_chunk_process(waveform, d, model_run, device, segment_size=256, hop_len
 
     # Convert final result back to a torch tensor for saving
     return torch.from_numpy(main_wav_np)
+
+
+# ############################################################################################################################
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+# License: MIT
+#
+# Convenience wrapper to perform STFT and iSTFT
+# ############################################################################################################################
+
+
+def spectro(x, n_fft=512, hop_length=None, pad=0):
+    *other, length = x.shape
+    x = x.reshape(-1, length)
+    is_mps = x.device.type == 'mps'
+    if is_mps:
+        x = x.cpu()
+    z = torch.stft(x,
+                   n_fft * (1 + pad),
+                   hop_length or n_fft // 4,
+                   window=torch.hann_window(n_fft).to(x),
+                   win_length=n_fft,
+                   normalized=True,
+                   center=True,
+                   return_complex=True,
+                   pad_mode='reflect')
+    _, freqs, frame = z.shape
+    return z.view(*other, freqs, frame)
+
+
+def ispectro(z, hop_length=None, length=None, pad=0):
+    *other, freqs, frames = z.shape
+    n_fft = 2 * freqs - 2
+    z = z.view(-1, freqs, frames)
+    win_length = n_fft // (1 + pad)
+    is_mps = z.device.type == 'mps'
+    if is_mps:
+        z = z.cpu()
+    x = torch.istft(z,
+                    n_fft,
+                    hop_length,
+                    window=torch.hann_window(win_length).to(z.real),
+                    win_length=win_length,
+                    normalized=True,
+                    length=length,
+                    center=True)
+    _, length = x.shape
+    return x.view(*other, length)
